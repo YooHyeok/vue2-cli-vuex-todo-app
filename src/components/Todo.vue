5d630e61311995c1fb5ca4633bce282da69a5776
webdevyoo <yjk7454@naver.com>
2024-10-17 00:12:27 +0900

    feat: `Todo.vue` - todo list 아이템 html 태그 요소 컴포넌트화

---
+++ src/components/Todo.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <input type="checkbox">
+    <span class="ml-3">Buy a car</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Todo',
+
+  data() {
+    return {
+      
+    };
+  },
+
+  mounted() {
+    
+  },
+
+  methods: {
+
+  },
+};
+</script>
