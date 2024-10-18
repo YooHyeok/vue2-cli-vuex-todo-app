bf3a91fc415a437cfe9d5a4a4d154f7ca1ec7e62
webdevyoo <yjk7454@naver.com>
2024-10-19 04:19:14 +0900

    feat: `TodoList.vue` - 반복되는 Todo를 출력해줄 랩핑용 컴포넌트 추가

---
+++ src/components/TodoList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <Todo 
+      v-for="todo in todos" 
+      :key="todo.id"
+      :todo="todo"
+      @toggle-checkbox="toggleCheckbox"
+      @toggle-delete="deleteTodo"
+    />
+  </div>
+</template>
+
+<script>
+import Todo from '@/components/Todo.vue'
+
+export default {
+  name: 'TodoList',
+  components: {
+    Todo 
+  },
+  props: {
+    todos: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+    toggleCheckbox(value) {
+      this.$emit('toggle-checkbox', value)
+    },
+    deleteTodo(todoId) {
+      this.$emit('toggle-delete', todoId)
+    },
+  },
+};
+</script>
