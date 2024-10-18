ef04d62bd38eab221fe9fdb77520a1b177571d3b
webdevyoo <yjk7454@naver.com>
2024-10-19 03:44:25 +0900

    design: `Todo.vue` - TODO 영역 간격 조정

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,13 +1,18 @@
 <template>
-  <div>
-    <input type="checkbox" :checked="todo.checked" @input="toggleCheckbox">
-    <span 
-      class="ml-3"
-      :class="todo.checked ? 'text-muted' : ''"
-      :style="todo.checked ? 'text-decoration: line-through' : ''"
-    >
-      {{ todo.text }}
-    </span>
+  <div class="mb-2 d-flex">
+    <div>
+      <input type="checkbox" 
+        :checked="todo.checked" 
+        @input="toggleCheckbox"
+      >
+    </div>
+      <span 
+        class="ml-3 flex-grow-1"
+        :class="todo.checked ? 'text-muted' : ''"
+        :style="todo.checked ? 'text-decoration: line-through' : ''"
+      >
+        {{ todo.text }}
+      </span>
   </div>
 </template>
 
@@ -36,7 +41,7 @@ export default {
         id: this.todo.id,
         checked: e.target.checked
       })
-    }
+    },
   },
 };
 </script>
