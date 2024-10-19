e4a9a7114685f30e1ad6550b096652133ee1115f
webdevyoo <yjk7454@naver.com>
2024-10-20 00:44:38 +0900

    feat: `AddTodo.vue` -  todo 입력 영역 컴포넌트화

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,26 @@
+<template>
+  <input 
+    type="text" 
+    class="w-100 p-2" 
+    placeholder="Type todo"
+    @keyup.enter="addTodo"
+    v-model="todoText"
+  >
+</template>
+
+<script>
+export default {
+  name: 'AddTodo',
+  data() {
+    return {
+      todoText: null,
+    };
+  },
+  methods: {
+    addTodo(e) {
+      this.$emit('add-todo', e.target.value)
+      this.todoText = null
+    },
+  },
+};
+</script>
