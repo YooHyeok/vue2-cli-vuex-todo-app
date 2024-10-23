1fd51a1d7a9af921f3fcf3b024dd9ba549abef8b
webdevyoo <yjk7454@naver.com>
2024-10-24 00:03:02 +0900

    feat: `App.vue` - vuex mutation 적용으로 기존 emit, methods속성 제거

--- src/vuex/App.vue
+++ src/vuex/App.vue
@@ -2,14 +2,9 @@
   <div id="app" class="container">
     <h1 class="text-center">Todo App</h1>
     <CompletedTodo/>
-    <AddTodo 
-      @add-todo="addTodo"
-    />
+    <AddTodo />
     <hr>
-    <TodoList 
-      @toggle-checkbox="toggleCheckbox"
-      @toggle-delete="deleteTodo"
-    />
+    <TodoList />
   </div>
 </template>
 
@@ -22,23 +17,5 @@ export default {
   components: {
     TodoList, AddTodo, CompletedTodo
   },
-  methods: {
-    addTodo(value) {
-      this.todos.push({
-        id: Math.random(),
-        text: value,
-        checked: false
-      })
-    },
-    toggleCheckbox({id, checked}) {
-      const index = this.todos.findIndex(todo => todo.id===id);
-      this.todos[index].checked = checked;
-      console.log(this.todos)
-    },
-    deleteTodo(id) {
-      const index = this.todos.findIndex(todo => todo.id===id);
-      this.todos.splice(index, 1)
-    }
-  }
 }
 </script>
