03cc9e1894affaf2173938e206252785f81f4fbc
webdevyoo <yjk7454@naver.com>
2024-10-25 01:02:46 +0900

    refactor: `store/index.js` - actions:fetchAll, mutation:FETCH_ALL 리팩토링

--- src/store/index.js
+++ src/store/index.js
@@ -7,13 +7,13 @@ export default new Vuex.Store({
     todos: [
       { id: 1, text: 'buy a car', checked: false},
       { id: 2, text: 'play a game', checked: false},
-    ]
+    ],
   },
   mutations: { // state 접근 및 변경 함수 정의
     FETCH_ALL(state, payload) {
-      const {taskAResponse, taskBResponse} = payload;
-      todos.push([...taskAResponse, ...taskBResponse])
-      console.log(todos)
+      const {taskAResult, taskBResult} = payload;
+      // todos.push([...taskAResponse, ...taskBResponse])
+      console.log([taskAResult, taskBResult])
     },
     ADD_TODO(state, payload) {
       console.log("payload: ", payload)
@@ -36,10 +36,17 @@ export default new Vuex.Store({
     },
   },
   actions: { // 비동기 작업 후 state 변경
-    fetchAll() {
-      Promise.all([fetch(`/task-a/${payload}`), fetch(`/task-b/${payload}`)])
+    fetchAll(context, payload) {
+      const {commit, dispatch} = context;
+      const URL = 'http://jsonplaceholder.typicode.com/todos'
+      Promise.all([fetch(`${URL}/${payload}`), fetch(`${URL}/${payload+1}`)])
       .then(([taskAResponse, taskBResponse]) => {
-        commit('FETCH_ALL', {taskAResponse, taskBResponse}); //state 변경
+        Promise.all([taskAResponse.json(), taskBResponse.json()])
+        .then(([taskAResult, taskBResult]) => {
+          console.log(taskAResult)
+          console.log(taskBResult)
+          commit('FETCH_ALL', {taskAResult, taskBResult}); //state 변경
+        })
       })
       .catch(e => {
         console.error('[ERROR][actions]Error in one of the tasks:', e);
@@ -48,6 +55,7 @@ export default new Vuex.Store({
     addTodo(context, payload) {
       const {commit, dispatch} = context;
       /* 비동기 작업 ex) axios(2초 소요) 후 commit 호출 */
+      dispatch('fetchAll', 1); // action 함수에서 다른 action 함수를 호출할 경우 context의 dispatch를 사용
       setTimeout(function() {
         commit('ADD_TODO', payload);
       }, 2000) //2초 후 실행
