e3556ce62a45b463474a0db4e411860556df56d4
webdevyoo <yjk7454@naver.com>
2024-10-26 02:27:47 +0900

    feat: `UserList.vue` - vuex actions getUsers 호출(dispatch) 및 템플릿 할당

---
+++ src/vuex/components/UserList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    UserList
+    <div v-for="user in people" :key="user.id">
+      {{ user.name }}
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'UserList',
+
+  computed: {
+    users() {
+      return this.$store.state.users;
+    }
+  },
+  created() {
+    this.getUsers();
+  },
+  methods: {
+    getUsers() {
+      this.$store.dispatch('getUsers');
+    }
+  }
+};
+</script>
