48d5c38048589f212f9375f61367f9c4f96f7450
webdevyoo <yjk7454@naver.com>
2024-10-26 02:47:00 +0900

    feat: `UserList.vue` - vuex map helper - mapState 적용

--- src/vuex/components/UserList.vue
+++ src/vuex/components/UserList.vue
@@ -1,21 +1,24 @@
 <template>
   <div>
     UserList
-    <div v-for="user in people" :key="user.id">
+    <div v-for="user in users" :key="user.id">
       {{ user.name }}
     </div>
   </div>
 </template>
 
 <script>
+import { mapActions, mapState } from 'vuex';
 
 export default {
   name: 'UserList',
 
   computed: {
-    users() {
+    /* users() { // map Helper 적용 전
       return this.$store.state.users;
-    }
+    } */
+    ...mapState(['users']), //map Helper 적용 - store의 state를 현재 컴포넌트의 computed 속성에 적용
+
   },
   created() {
     this.getUsers();
