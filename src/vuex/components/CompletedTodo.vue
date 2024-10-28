2cc7a2564bed4ea744e000c311d81beb38b37b13
webdevyoo <yjk7454@naver.com>
2024-10-29 00:41:17 +0900

    feat: `CompletedTodo.vue` - todo modules getters 호출 적용

--- src/vuex/components/CompletedTodo.vue
+++ src/vuex/components/CompletedTodo.vue
@@ -13,7 +13,8 @@ export default {
     }, */
     numberOfCompletedTodo() {
       // return this.todos.filter(todo => todo.checked).length // getters 적용 전
-      return this.$store.getters.numberOfCompletedTodo // getters 적용
+      // return this.$store.getters.numberOfCompletedTodo // getters 적용
+      return this.$store.getters['todo/numberOfCompletedTodo'] // modules 적용
     }
   },
 };
